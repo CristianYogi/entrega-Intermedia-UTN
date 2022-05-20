64dd4fd5a9bf6b732cc0cc672d02e49f1e8d880d
CristianYogi <yogi@live.com.ar>
2022-05-20 11:51:48 -0300

    cambios

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const server = express()
 const path = require('path')
 
-const port = process.env.port
+const port = process.env.PORT
 
 server.use(express.static('public'))
 
