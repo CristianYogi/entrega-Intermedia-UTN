bf8ff50927fcb572e93bf38c73b144aafa5df208
CristianYogi <yogi@live.com.ar>
2022-05-20 11:45:15 -0300

    Inicio

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+
+const server = express()
+const path = require('path')
+
+const port = process.env.port
+
+server.use(express.static('public'))
+
+
+server.set('views', path.join(__dirname, 'views'))
+server.set('view engine', 'ejs');
+
+
+server.get("/", (req, res) => {
+    res.send("<h1>Mi API</h1>")
+})
+
+
+
+server.listen(port, (err) => {
+    err ? console.log(`Error: ${err}`) : console.log(`App corre en http://localhost:${port}`)
+})
+
